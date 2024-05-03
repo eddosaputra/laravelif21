1c29a4e18a4ff8f876c3c560eecba98f94df096d
eddosaputra <eddosaputra_2327250007@mhs.mdp.ac.id>
2024-05-03 20:33:24 +0700

    .

---
+++ if21-app/resources/views/fakultas/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.main')
+@section('title', 'Tambah Fakultas')
+@section('content')
+    <h2>Tambah Fakultas</h2>
+    <p>Ini halaman tambah fakultas</p>
+@endsection
+
---
+++ if21-app/resources/views/fakultas/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fakultas</title>
+</head>
+<body>
+    <h1>Universitas MDP</h1>
+    <a href="{{ url('fakultas') }}">List</a> | <a href="{{ url('fakultas/create') }}">Tambah</a>
+    <h2>Daftar Fakultas</h2>
+    <p>Ini halaman daftar fakultas</p>
+    
+</body>
+</html>
