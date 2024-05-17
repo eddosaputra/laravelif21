59565c533b323d2bdec4cd94277d42be1aba0190
eddosaputra <eddosaputra_2327250007@mhs.mdp.ac.id>
2024-05-17 20:32:16 +0700

    menambahkan template

--- if21-app/resources/views/fakultas/index.blade.php
+++ if21-app/resources/views/fakultas/index.blade.php
@@ -1,12 +1,38 @@
 @extends('layout.main')
 @section('title', 'Daftar Fakultas')
 @section('content')
-    <h2>Daftar Fakultas</h2>
-    <p>Ini halaman daftar fakultas</p>
 
-    @foreach ($fakultas as $item)
-        {{  $item['nama'] }} {{ $item['singkatan'] }} <br>
-    @endforeach
+<div class="row">
+<div class="col-lg-12 grid-margin stretch-card">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Daftar Fakultas</h4>
+            <p class="card-description">
+            List data fakultas
+            </p>
+            <a href="{{ url('fakultas/create') }}" class="btn btn-primary btn-rounded btn-fw">Tambah</a>
+            <div class="table-responsive">
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th>Nama Fakultas</th>
+                    <th>Singkatan</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($fakultas as $item)
+                    <tr>
+                        <td>{{ $item['nama'] }}</td>
+                        <td>{{ $item['singkatan'] }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 @endsection
 
 
--- if21-app/resources/views/prodi/index.blade.php
+++ if21-app/resources/views/prodi/index.blade.php
@@ -1,10 +1,38 @@
 @extends('layout.main')
 @section('title', 'Daftar Fakultas')
 @section('content')
-    <h2>Daftar Program Studi</h2>
-    <p>Ini halaman daftar Program Studi</p>
-
-    @foreach ($prodi as $item)
-        {{  $item['nama'] }} {{  $item['fakultas'] ['nama'] }}  {{  $item['fakultas'] ['singkatan'] }} <br>
-    @endforeach
+<div class="row">
+    <div class="col-lg-12 grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Daftar Program Studi</h4>
+            <p class="card-description">
+            List program studi
+            </p>
+            <a href="{{ url('prodi/create') }}" class="btn btn-primary btn-rounded btn-fw">Tambah</a>
+            <div class="table-responsive">
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th>Nama Program Studi</th>
+                    <th>Nama Fakultas</th>
+                    <th>Singkatan</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($prodi as $item)
+                    <tr>
+                        <td>{{ $item['nama'] }}</td>
+                        <td>{{ $item['fakultas'] ['nama'] }}</td>
+                        <td>{{ $item['fakultas'] ['singkatan'] }}</td>
+                    </tr>
+                    @endforeach
+                  
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 @endsection
