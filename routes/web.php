717edd2fcd0ab4d9008be54d22bf25a60cdecfbc
eddosaputra <eddosaputra_2327250007@mhs.mdp.ac.id>
2024-06-21 20:24:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.loginnew');
 });
 Route::resource('fakultas', FakultasController::class);
 Route::resource('prodi', ProdiController::class);
